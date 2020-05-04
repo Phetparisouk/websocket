a5275b424a521b244760c53b32dfdf429d3dec74
Phetparisouk <phetparisouk.souvannasacd@limayrac.fr>
2020-05-04 12:21:53 +0200

    step 1 - webserver

--- websocket-server.js
+++ websocket-server.js
@@ -1,1 +1,7 @@
 const WebSocket = require('ws');
+
+const myWebSocketServer = new WebSocket.Server({ port: process.env.WS_PORT });
+
+myWebSocketServer.on('connection', webSocket => {
+    console.log('connection established');
+});
